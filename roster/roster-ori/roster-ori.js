ebf87ed6c68781600216d95ff9c8923f94bd678b
Yaya Project <117073849+YayaProject@users.noreply.github.com>
2024-08-05 15:52:50 +0800

    Add files via upload

---
+++ roster/roster-ori/roster-ori.js
@@ -0,0 +1,108 @@
+// HARI SENIN
+const jam1Senin = "UPACARA";
+const jam2Senin = "MTK MINAT";
+const jam3Senin = "MTK MINAT";
+const jam4Senin = "KIMIA";
+const jam5Senin = "MTK WAJIB";
+const jam6Senin = "MTK WAJIB";
+const jam7Senin = "SENI BUDAYA";
+const jam8Senin = "SENI BUDAYA";
+const jam9Senin = "BIOLOGI";
+const jam10Senin = "BIOLOGI";
+
+// HARI SELASA
+const jam1Selasa = "FISIKA";
+const jam2Selasa = "FISIKA";
+const jam3Selasa = "B.INDONESIA";
+const jam4Selasa = "B.INDONESIA";
+const jam5Selasa = "PAI";
+const jam6Selasa = "PAI";
+const jam7Selasa = "PAI";
+const jam8Selasa = "EKONOMI";
+const jam9Selasa = "EKONOMI";
+const jam10Selasa = "EKONOMI";
+
+// HARI RABU
+const jam1Rabu = "KIMIA";
+const jam2Rabu = "KIMIA";
+const jam3Rabu = "KIMIA";
+const jam4Rabu = "PENJAS";
+const jam5Rabu = "PENJAS";
+const jam6Rabu = "PENJAS";
+const jam7Rabu = "FISIKA";
+const jam8Rabu = "FISIKA";
+const jam9Rabu = "B.INGGRIS";
+const jam10Rabu = "B.INGGRIS";
+
+// HARI KAMIS
+const jam1Kamis = "MTK WAJIB";
+const jam2Kamis = "MTK WAJIB";
+const jam3Kamis = "PPKN";
+const jam4Kamis = "PPKN";
+const jam5Kamis = "BIOLOGI";
+const jam6Kamis = "BIOLOGI";
+const jam7Kamis = "B.INDONESIA";
+const jam8Kamis = "B.INDONESIA";
+const jam9Kamis = "MTK MINAT";
+const jam10Kamis = "MTK MINAT";
+
+// HARI JUMAT
+const jam1Jumat = "LITERASI ALQURAN/SENAM";
+const jam2Jumat = "PRAKARYA";
+const jam3Jumat = "PRAKARYA";
+const jam4Jumat = "EKONOMI";
+const jam5Jumat = "BK/KEAGAMAAN";
+const jam6Jumat = "SEJARAH INDO";
+const jam7Jumat = "SEJARAH INDO";
+
+document.getElementById("jam1Senin").innerText = jam1Senin;
+document.getElementById("jam2Senin").innerText = jam2Senin;
+document.getElementById("jam3Senin").innerText = jam3Senin;
+document.getElementById("jam4Senin").innerText = jam4Senin;
+document.getElementById("jam5Senin").innerText = jam5Senin;
+document.getElementById("jam6Senin").innerText = jam6Senin;
+document.getElementById("jam7Senin").innerText = jam7Senin;
+document.getElementById("jam8Senin").innerText = jam8Senin;
+document.getElementById("jam9Senin").innerText = jam9Senin;
+document.getElementById("jam10Senin").innerText = jam10Senin;
+
+document.getElementById("jam1Selasa").innerText = jam1Selasa;
+document.getElementById("jam2Selasa").innerText = jam2Selasa;
+document.getElementById("jam3Selasa").innerText = jam3Selasa;
+document.getElementById("jam4Selasa").innerText = jam4Selasa;
+document.getElementById("jam5Selasa").innerText = jam5Selasa;
+document.getElementById("jam6Selasa").innerText = jam6Selasa;
+document.getElementById("jam7Selasa").innerText = jam7Selasa;
+document.getElementById("jam8Selasa").innerText = jam8Selasa;
+document.getElementById("jam9Selasa").innerText = jam9Selasa;
+document.getElementById("jam10Selasa").innerText = jam10Selasa;
+
+document.getElementById("jam1Rabu").innerText = jam1Rabu;
+document.getElementById("jam2Rabu").innerText = jam2Rabu;
+document.getElementById("jam3Rabu").innerText = jam3Rabu;
+document.getElementById("jam4Rabu").innerText = jam4Rabu;
+document.getElementById("jam5Rabu").innerText = jam5Rabu;
+document.getElementById("jam6Rabu").innerText = jam6Rabu;
+document.getElementById("jam7Rabu").innerText = jam7Rabu;
+document.getElementById("jam8Rabu").innerText = jam8Rabu;
+document.getElementById("jam9Rabu").innerText = jam9Rabu;
+document.getElementById("jam10Rabu").innerText = jam10Rabu;
+
+document.getElementById("jam1Kamis").innerText = jam1Kamis;
+document.getElementById("jam2Kamis").innerText = jam2Kamis;
+document.getElementById("jam3Kamis").innerText = jam3Kamis;
+document.getElementById("jam4Kamis").innerText = jam4Kamis;
+document.getElementById("jam5Kamis").innerText = jam5Kamis;
+document.getElementById("jam6Kamis").innerText = jam6Kamis;
+document.getElementById("jam7Kamis").innerText = jam7Kamis;
+document.getElementById("jam8Kamis").innerText = jam8Kamis;
+document.getElementById("jam9Kamis").innerText = jam9Kamis;
+document.getElementById("jam10Kamis").innerText = jam10Kamis;
+
+document.getElementById("jam1Jumat").innerText = jam1Jumat;
+document.getElementById("jam2Jumat").innerText = jam2Jumat;
+document.getElementById("jam3Jumat").innerText = jam3Jumat;
+document.getElementById("jam4Jumat").innerText = jam4Jumat;
+document.getElementById("jam5Jumat").innerText = jam5Jumat;
+document.getElementById("jam6Jumat").innerText = jam6Jumat;
+document.getElementById("jam7Jumat").innerText = jam7Jumat;
